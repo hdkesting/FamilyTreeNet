85493e5e15a29da164128ea9a5660cf3fedf7b72
Hans Kesting <hans_kesting@epam.com>
2019-09-02 14:34:54 +0200

    Basic changes in UI, added empty projects to be extended

---
+++ FamilyTreeNet/Pages/Reports/Summary.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FamilyTreeNet.Pages.Reports
+{
+    public class SummaryModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
